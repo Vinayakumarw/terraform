a69c40d822532c3482f08cb565e8928e102f5dd5
Vinayakumarw <vinayakumar1995w@gmail.com>
2025-04-15 23:45:42 -0700

    removed

--- task.tf
+++
@@ -1,6 +0,0 @@
-resource "local_file" "vinay" {
-  count    = 3
-  filename = "jaya/vinay-${count.index}.txt"
-  content  = "this file is a demo file use for creation of a local resource modification"
-
-}
